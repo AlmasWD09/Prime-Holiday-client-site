7ba1b4f7c7cd7ee5099b4eda29e05bb0363d2d7d
Almas Hossain <almashossain7384@gmail.com>
2025-01-11 14:47:48 +0600

    scroll button design and functionality update

--- src/app/components/shared/ScrollTopButton.jsx
+++ src/app/components/shared/ScrollTopButton.jsx
@@ -1,30 +1,7 @@
 "use client";
 import { IoIosArrowUp } from "react-icons/io";
-import { useEffect, useState } from "react";
 
 const ScrollTopButton = () => {
-    const [showButton, setShowButton] = useState(false);
-
-    const handleScroll = () => {
-        const footer = document.querySelector("footer"); 
-        if (footer) {
-            const footerTop = footer.offsetTop; 
-            const footerHeight = footer.offsetHeight;
-            const scrollPosition = window.scrollY + window.innerHeight;
-
-            // Show button when scrolling in the footer area
-            if (scrollPosition >= footerTop && scrollPosition <= footerTop + footerHeight) {
-                setShowButton(true); 
-            } else {
-                setShowButton(false); // Hide button when not in the footer area
-            }
-        }
-    };
-
-    useEffect(() => {
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
 
     const top = () => {
         window.scrollTo({
@@ -35,14 +12,12 @@ const ScrollTopButton = () => {
 
     return (
         <>
-            {showButton && (
-                <div
-                    onClick={top}
-                    className="w-[40px] h-[40px] md:w-[50px] md:h-[50px] rounded-xl border-2 border-primary hover:bg-primary flex justify-center items-center text-center text-primary hover:text-white fixed right-[16px] lg:right-[120px] bottom-[188px] md:bottom-[120px] cursor-pointer z-40"
-                >
-                    <IoIosArrowUp className="text-3xl flex justify-center items-center text-center" />
-                </div>
-            )}
+            <div
+                onClick={top}
+                className="w-[40px] h-[40px] md:w-[50px] md:h-[50px] rounded-xl border-2 border-primary  flex justify-center items-center text-center text-primary cursor-pointer z-40"
+            >
+                <IoIosArrowUp className="text-3xl flex justify-center items-center text-center" />
+            </div>
         </>
     );
 };
