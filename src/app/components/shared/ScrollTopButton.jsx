20ce4ca2954e025e2f749a923c40b8ceb9b6ea39
Almas Hossain <almashossain7384@gmail.com>
2025-01-11 11:24:24 +0600

    footer heading text  update

--- src/app/components/shared/ScrollTopButton.jsx
+++ src/app/components/shared/ScrollTopButton.jsx
@@ -30,7 +30,7 @@ const ScrollTopButton = () => {
     }
     return (<>
         {
-            backTopBtn && <div onClick={top} className="w-[40px] h-[40px] md:w-[50px] md:h-[50px] rounded-xl border-2 border-primary  hover:bg-primary  flex justify-center items-center text-center text-primary hover:text-white fixed right-[16px] lg:right-[120px] bottom-[188px] md:bottom-[120px] cursor-pointer z-40">
+            backTopBtn && <div onClick={top} className=" w-[40px] h-[40px] md:w-[50px] md:h-[50px] rounded-xl border-2 border-primary  hover:bg-primary  flex justify-center items-center text-center text-primary hover:text-white fixed right-[16px] lg:right-[120px] bottom-[188px] md:bottom-[120px] cursor-pointer z-40">
                 <IoIosArrowUp className="text-3xl flex justify-center items-center text-center" /></div>
 
         }
@@ -40,3 +40,6 @@ const ScrollTopButton = () => {
 };
 
 export default ScrollTopButton;
+
+
+
