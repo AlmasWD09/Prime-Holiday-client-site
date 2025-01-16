5b268e8d2aeea7fa46a773b2b2f45a85220fd83b
Almas Hossain <almashossain7384@gmail.com>
2025-01-16 10:05:15 +0600

    footer text update

--- src/app/components/dashboard/table/AddDestinationTable.jsx
+++ src/app/components/dashboard/table/AddDestinationTable.jsx
@@ -1,22 +1,26 @@
 import { Select, Input, Button, Form, Upload } from "antd";
+import TextArea from "antd/es/input/TextArea";
+import axios from "axios";
 import { useEffect } from "react";
 
 
 
 const AddDestinationTable = () => {
     const [form] = Form.useForm();
-    const countries = ["Asia", "Africa", "North America", "Antarctica", "Antarctica", "Europe", "Oceania"]
+    // const countries = ["Asia", "Africa", "North America", "Antarctica", "Antarctica", "Europe", "Oceania"]
 
     // handle form submit
     const handleSubmit = (values) => {
         const data = {
             ...values,
         };
-        console.log(data)
         form.resetFields()
+
+
+        const res = axios.post('http://10.0.80.13:8000/api/admin/destination/store', data)
+        console.log(res, 'line 21')
     };
 
-    // fetch('http://10.0.80.13:8000/api/admin/destination/store',data)
 
     return (
         <div className="space-y-4">
@@ -34,49 +38,32 @@ const AddDestinationTable = () => {
                     </Form.Item>
                 </div>
 
-
-                {/* title */}
+                {/* days */}
                 <div className="mb-2">
-                    <p>Title</p>
+                    <p>Days</p>
                     <Form.Item
-                        name="title"
+                        name="day"
                         rules={[{ required: true, message: "Please enter the title!" }]}
                     >
-                        <Input placeholder="Enter the country title" className="max-w-sm" />
+                        <Input type="number" placeholder="Enter the country title" className="max-w-sm" />
                     </Form.Item>
                 </div>
 
-                
 
-                {/* select  */}
-                <div className="mb-2">
-                    <p>Select the continent</p>
+                {/* Description */}
+                <div className="mb-2 max-w-sm">
+                    <p>Description</p>
                     <Form.Item
-                        name="continent"
-                        rules={[{ required: true, message: "Please select a continent!" }]}
+                        name="description"
+                        rules={[{ required: true, message: "Please enter a description!" }]}
                     >
-                        <Select placeholder="Select the continent" className="max-w-sm">
-                            {countries.map((country, index) => (
-                                <Select.Option key={index} value={country}>
-                                    {country}
-                                </Select.Option>
-                            ))}
-                        </Select>
-                    </Form.Item>
-
-                </div>
-
-                {/* country name */}
-                <div className="mb-2">
-                    <p>Country Name</p>
-                    <Form.Item
-                        name="countryName"
-                        rules={[{ required: true, message: "Please enter the country name!" }]}
-                    >
-                        <Input placeholder="Enter the country name" className="max-w-sm" />
+                        <TextArea
+                            placeholder="Enter a description of the destination"
+                            rows={4}
+                        // className="max-w-sm"
+                        />
                     </Form.Item>
                 </div>
-
 
                 {/* image upload */}
                 <div className="mb-2">
@@ -94,6 +81,40 @@ const AddDestinationTable = () => {
                         </Upload>
                     </Form.Item>
                 </div>
+
+                {/* Price */}
+                <div className="mb-2">
+                    <p>Price</p>
+                    <Form.Item
+                        name="price"
+                        rules={[{ required: true, message: "Please enter the price!" }]}
+                    >
+                        <Input
+                            placeholder="Enter the price"
+                            className="max-w-sm"
+                            type="number"
+                            min={0}
+                        />
+                    </Form.Item>
+                </div>
+
+                {/* select  */}
+                {/* <div className="mb-2">
+                    <p>Select the continent</p>
+                    <Form.Item
+                        name="continent"
+                        rules={[{ required: true, message: "Please select a continent!" }]}
+                    >
+                        <Select placeholder="Select the continent" className="max-w-sm">
+                            {countries.map((country, index) => (
+                                <Select.Option key={index} value={country}>
+                                    {country}
+                                </Select.Option>
+                            ))}
+                        </Select>
+                    </Form.Item>
+
+                </div> */}
 
                 {/* button */}
                 <div className="mb-2">
