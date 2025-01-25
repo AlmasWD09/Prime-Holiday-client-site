56d0cfb02597a671b23cf833f2e9341c9141fc46
Almas Hossain <almashossain7384@gmail.com>
2025-01-25 13:12:31 +0600

    token remove successfully

--- src/app/admin/dashboard/page.js
+++ src/app/admin/dashboard/page.js
@@ -1,12 +1,13 @@
-import { FaRegEye } from "react-icons/fa";
+
 
 const DashboardPage = () => {
   return (
-    <div className="">
-     
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-center text-5xl font-Roboto font-bold text-primary">
+        <h1>Wellcome !!</h1>
+      <p>Prime Holiday Dashboard</p></div>
     </div>
 
   )
 }
-
 export default DashboardPage
